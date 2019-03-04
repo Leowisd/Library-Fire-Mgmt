4506f3c30bab5a151b35978cefaae1db794149f7
Leowisd <32425638+Leowisd@users.noreply.github.com>
2019-03-04 15:33:15 -0600

    20190304
    Fire Mgmt Library Initialization

---
+++ LandisLibraryFire/LandisLibraryFire/FireExtensionMain.cs
@@ -0,0 +1,18 @@
+﻿using Landis.Core;
+using Landis.SpatialModeling;
+
+namespace Landis.Library.FireManagement
+{
+    public abstract class FireExtensionMain: ExtensionMain
+    {
+        /// <summary>
+        /// The extension type for fire extensions.
+        /// </summary>
+        public static readonly ExtensionType ExtType = new ExtensionType("disturbance:fire");
+
+        public FireExtensionMain(string name)
+            : base(name, ExtType)
+        {
+        }
+    }
+}
